f1c62795222a854544cc9ae1966a5a9d69a4dfa4
saadtalal1 <saadtalal0112@gmail.com>
2026-01-04 13:39:37 +0400

    Add files via upload

---
+++ ACP.py
@@ -0,0 +1,11 @@
+num1=int(input("Please enter the first number: "))
+num2=int(input("Please enter the second number: "))
+num3=int(input("Please enter the third number: "))
+temp=num1
+num1=num3
+temp2=num2
+num2=temp
+num3=temp2
+print("The value of num1 is",num1)
+print("The value of num2 is",num2)
+print("The value of num3 is",num3)
